548596604933032d53825b2bc6a6e9ee17eb23ff
sheynz <loppesofc@gmail.com>
2022-09-07 22:57:07 -0300

    testando

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const { TelegramClient } = require("telegram");
+const { StringSession } = require("telegram/sessions");
+const input = require("input"); // npm i input
+
+const apiId = 566;
+const apiHash = "";
+const stringSession = new StringSession(""); // preencha isso mais tarde com o valor de session.save()
+
+(async () => {
+  console.log("Loading interactive example...");
+  const client = new TelegramClient(stringSession, apiId, apiHash, {
+    connectionRetries: 5,
+  });
+  await client.start({
+    phoneNumber: async () => await input.text("Please enter your number: "),
+    password: async () => await input.text("Please enter your password: "),
+    phoneCode: async () =>
+      await input.text("Please enter the code you received: "),
+    onError: (err) => console.log(err),
+  });
+  console.log("You should now be connected.");
+  console.log(client.session.save()); // Save this string to avoid logging in again
+  await client.sendMessage("me", { message: "Hello!" });
+})();
